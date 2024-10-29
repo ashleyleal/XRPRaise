8978cff6fe0ba13d1b3af14649fd7519c11521bd
chocoboash <anleal2004@gmail.com>
2024-10-28 23:29:21 -0400

    chore: init

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const {REST} = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({ 
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+});
+
+// List of all commands
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+}
+
+client.on("ready", () => {
+    const guild_ids = client.guilds.cache.map(guild => guild.id);
+    const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+    for (const guild_id of guild_ids) {
+        rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guild_id), { body: commands })
+            .then(() => console.log(`Successfully registered commands for guild ${guild_id}`))
+            .catch(console.error);
+    }
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (!client.commands.has(commandName)) return;
+
+    try {
+        await client.commands.get(commandName).execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+client.login(process.env.TOKEN);
