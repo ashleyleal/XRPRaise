7f0c2d04595866f180fd87f90414ab2a869ccedb
chocoboash <anleal2004@gmail.com>
2024-10-31 02:47:05 -0400

    fix: improve end campaign command by checking if already deleted and checking permission before ending

--- commands/end_campaign.js
+++ commands/end_campaign.js
@@ -11,19 +11,25 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         const campaignName = interaction.options.getString('campaign_name');
+        const userId = interaction.user.id; 
 
         try {
             const campaign = await Campaign.findOne({ name: campaignName });
-            const campaignSuccess = campaign.currentAmount >= campaign.goal;
 
             if (!campaign) {
                 return interaction.reply({ content: 'Campaign not found.', ephemeral: true });
             }
+            if (campaign.creatorId !== userId) {
+                return interaction.reply({ content: 'You do not have permission to end this campaign.', ephemeral: true });
+            }
 
-            campaign.isComplete = true;
+            if (campaign.isComplete) {
+                return interaction.reply({ content: 'This campaign has already ended.', ephemeral: true });
+            }
+            campaign.isComplete = true; 
             await campaign.save();
 
-            await interaction.reply(`Successfully ended the campaign "${campaign.name}". Total amount raised: ${campaign.currentAmount} XRP/${campaign.goal} XRP. Campaign was a ${campaignSuccess ? 'success' : 'failure'}.`);
+            await interaction.reply(`Successfully ended the campaign "${campaign.name}". Total amount raised: ${campaign.currentAmount} XRP/${campaign.goal} XRP.`);
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: 'There was an error ending the campaign.', ephemeral: true });
